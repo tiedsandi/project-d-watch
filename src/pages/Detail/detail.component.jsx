f3eb142534f54a7b4a90c6376e2c528941853ced
tiedSandi <wkwkwkand@gmail.com>
2023-07-05 20:56:24 +0700

    test detail bug

--- src/pages/Detail/detail.component.jsx
+++ src/pages/Detail/detail.component.jsx
@@ -1,4 +1,4 @@
-import React, { Component, useEffect } from "react";
+import React, { useEffect } from "react";
 import { ReactComponent as BackIcon } from "../../assets/icons/back.svg";
 import "./detail.style.scss";
 import { useNavigate, useParams } from "react-router-dom";
@@ -25,6 +25,9 @@ const Detail = () => {
 	const directing = useSelector(directingArr);
 	const isLoadingDetail = useSelector(loadingDetail);
 	const isLoadingCast = useSelector(loadingCast);
+	console.log(detail);
+	console.log(writer);
+	console.log(directing);
 
 	useEffect(() => {
 		dispatch(fetchDetailStartAsyncs(type, id));
@@ -39,57 +42,56 @@ const Detail = () => {
 			{isLoadingDetail && isLoadingCast ? (
 				<LoadingSection />
 			) : (
-				<Component>
-					{detail && writer && directing && (
-						<div
-							className="detail-container"
-							style={{
-								backgroundImage: `url(${
-									detail.backdrop_path
-										? "https://image.tmdb.org/t/p/original" +
-										  detail.backdrop_path
-										: ImgDefault
-								})`,
-							}}
-						>
-							<div className="detail-overlay">
-								<div onClick={goBack}>
-									<BackIcon className="back-button" />
-								</div>
-								<div className="detail-info">
-									<div className="detail-group right">
-										<h2 className="detail-heading">Directing</h2>
-										<h3 className="detail-subheading">
-											{directing.length > 0 ? directing[0].name : "-"}
-										</h3>
-									</div>
-									<div className="detail-group left">
-										<h2 className="detail-heading">Genre</h2>
-										<h3 className="detail-subheading">
-											{detail.genres.length > 0 ? detail.genres[0].name : "-"}
-										</h3>
-									</div>
-									<div className="detail-group right">
-										<h2 className="detail-heading">Writer</h2>
-										<h3 className="detail-subheading">
-											{writer.length > 0 ? writer[0].name : "-"}
-										</h3>
-									</div>
-									<div className="detail-title">
-										<h1>
-											{detail.title || detail.name}
-											{detail.original_title &&
-												detail.original_title !== detail.title &&
-												detail.original_title !== detail.name &&
-												"/" + detail.original_title}
-										</h1>
-										<p>{detail.overview}</p>
-									</div>
-								</div>
+				// <>
+				// 	{detail && writer && directing && (
+				<div
+					className="detail-container"
+					style={{
+						backgroundImage: `url(${
+							detail.backdrop_path
+								? "https://image.tmdb.org/t/p/original" + detail.backdrop_path
+								: ImgDefault
+						})`,
+					}}
+				>
+					<div className="detail-overlay">
+						<div onClick={goBack}>
+							<BackIcon className="back-button" />
+						</div>
+						<div className="detail-info">
+							<div className="detail-group right">
+								<h2 className="detail-heading">Directing</h2>
+								<h3 className="detail-subheading">
+									{/* {directing.length > 0 ? directing[0].name : "-"} */}
+								</h3>
+							</div>
+							<div className="detail-group left">
+								<h2 className="detail-heading">Genre</h2>
+								<h3 className="detail-subheading">
+									{/* {detail.genres.length > 0 ? detail.genres[0].name : "-"} */}
+								</h3>
+							</div>
+							<div className="detail-group right">
+								<h2 className="detail-heading">Writer</h2>
+								<h3 className="detail-subheading">
+									{/* {writer.length > 0 ? writer[0].name : "-"} */}
+								</h3>
+							</div>
+							<div className="detail-title">
+								<h1>
+									{detail.title || detail.name}
+									{detail.original_title &&
+										detail.original_title !== detail.title &&
+										detail.original_title !== detail.name &&
+										"/" + detail.original_title}
+								</h1>
+								<p>{detail.overview}</p>
 							</div>
 						</div>
-					)}
-				</Component>
+					</div>
+				</div>
+				// 	)}
+				// </>
 			)}
 		</>
 	);
